c918abd9b12314236c2ff1333d4c9c9522a17359
Li-au <auge.lilian75@gmail.com>
2026-06-24 14:14:21 +0200

    agrandir titan 1100 -> 1200

--- src/components/GameScreenMockup.jsx
+++ src/components/GameScreenMockup.jsx
@@ -277,7 +277,7 @@ export default function GameScreenMockup() {
 
           {/* Titan (behind the UI), bottom arc seated on the TAP button */}
           <div className="absolute left-1/2 -translate-x-1/2 bottom-[150px] pointer-events-none">
-            <BossArt src={bossImgUrl} emoji={boss.emoji} width={1100} />
+            <BossArt src={bossImgUrl} emoji={boss.emoji} width={1200} />
           </div>
 
           {/* Tap button (above the titan, at the bottom of the ring) */}
